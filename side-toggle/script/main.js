c95d008dc597c8b04762f0b5804147a48bff5e95
park-hyounga <undozz@daum.net>
2020-05-07 12:14:43 +0900

    사이드 토글 완료

---
+++ side-toggle/script/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".onoff_btn").click(function(){
+        $("#side").toggleClass("open");
+        if($("#side").hasClass("open")){
+            $(".onoff_btn").html("&lt;");
+        }else{
+            $(".onoff_btn").html("&gt;");
+        }
+    });
+});
