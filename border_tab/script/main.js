4d2f0f9d398e0099635332676aa98866f4937a8f
park-hyounga <undozz@daum.net>
2020-05-08 11:50:57 +0900

    팝업창 만들기

--- border_tab/script/main.js
+++ border_tab/script/main.js
@@ -3,13 +3,19 @@ $(function(){
     popup(); // popup 기능 호출
 });
 function tabs(){ // tabs 기능 정의
+    notice_on();
+    gallery_on(); 
+}
+function notice_on(){
     $(".board_title li").click(function(){
-        if(!$(this).hasClass("on")) {
-            $("#gallery").css("display","block");
+        if($(this).hasClass("on")) {
+
         } else {
-            $("#gallery").css("display","none");
+            $(this).siblings().removeClass("on");
+            $(this).addClass("on");
+            $("#gallery").toggleClass("hide");
         }
-    });        
+    });
 }
 function popup(){ // popup 기능 정의   
     popup_open();
